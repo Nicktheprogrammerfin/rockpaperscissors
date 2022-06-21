2646a4d85b3542f6ffb8c8296f5ec34f9a5e3318
Nicktheprogrammerfin <106001886+Nicktheprogrammerfin@users.noreply.github.com>
2022-06-21 16:13:43 -0500

    Update script.js
    Finished game, looping until player or computer score hits 3.

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
-    
-let gameMatches = 0;
+let round = 1;
 let playerScore = 0;
 let computerScore = 0;
 let winScore = 3;
 
-if (playerScore === winScore){
-    console.log("Congrats, you won the battle!")
-}
+function gamePlay(){
+    let playerSelection = prompt("Choose: Rock, Paper, or Scissors!");
+    let playerHand = playerSelection.toLowerCase();
+    console.log(`You chose: ${playerHand}`);
 
-if (computerScore === winScore){
-    console.log("Ooo, Tough break! The computer has won the battle!")
-}
+    const computerPlay = function(){
+    let hands = ['rock', 'paper', 'scissors'];
+    let computerChoice = hands[Math.floor(Math.random() * hands.length)];
+    return computerChoice;
+    };
 
+    let computerHand = computerPlay();
 
-let playerSelection = prompt("Choose: Rock, Paper, or Scissors!");
-let playerHand = playerSelection.toLowerCase();
-console.log(`You chose: ${playerHand}`);
+    console.log(`The computer chose: ${computerHand}`)
 
-const computerPlay = function(){
-let hands = ['rock', 'paper', 'scissors'];
-let computerChoice = hands[Math.floor(Math.random() * hands.length)];
-return computerChoice;
-};
-
-let computerHand = computerPlay();
-
-console.log(`The computer chose: ${computerHand}`)
-
-
-function gamePlay(playerHand, computerHand){
     if (computerHand === 'rock' && playerHand === 'scissor'){
         console.log("Sorry, you've lost! Rock beats scissors!")
+        computerScore++;
     } else if (computerHand === 'scissors' && playerHand === 'paper'){
         console.log("Sorry, you've lost! Scissors cuts paper!")
+        computerScore++;
     } else if (computerHand === 'paper' && playerHand === 'rock'){
         console.log("Sorry, you've lost! Paper covers rock!")
+        computerScore++;
     } else if (computerHand === playerHand){
         console.log("You've tied!")
     } else {
         console.log(`You won, ${playerHand} beats ${computerHand}!`)
+        playerScore++;
     };
-}
+    
+    round++;
 
-gamePlay();
+};
+
+while ((playerScore < winScore) || (computerScore < winScore)){
+    console.log(`You are on round ${round}`);
+    gamePlay();
+    console.log(`You have ${playerScore} points!`);
+    console.log(`The computer has ${computerScore} points!`)
+
+    if (playerScore === winScore){
+        console.log("Congrats, you won the battle! Refresh to play again!")
+    } else if (computerScore === winScore){
+        console.log("Ooo, Tough break! The computer has won the battle! Refresh to try again!")
+    }
+
+    if ((playerScore === winScore) || (computerScore === winScore)){
+        break;
+    }
+};
