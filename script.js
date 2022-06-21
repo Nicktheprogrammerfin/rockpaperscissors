b8ac5089958a2af26515a8557151f929c812d9c5
Nick <Nicktheprogrammerfin@gmail.com>
2022-06-20 21:54:43 -0500

    made baseline scores, need to increase numbers next

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
+    
+let gameMatches = 0;
+let playerScore = 0;
+let computerScore = 0;
+let winScore = 3;
+
+if (playerScore === winScore){
+    console.log("Congrats, you won the battle!")
+}
+
+if (computerScore === winScore){
+    console.log("Ooo, Tough break! The computer has won the battle!")
+}
+
+
+let playerSelection = prompt("Choose: Rock, Paper, or Scissors!");
+let playerHand = playerSelection.toLowerCase();
+console.log(`You chose: ${playerHand}`);
+
 const computerPlay = function(){
-    let hands = ['rock', 'paper', 'scissors'];
-    let computerChoice = hands[Math.floor(Math.random() * hands.length)];
-    return computerChoice;
+let hands = ['rock', 'paper', 'scissors'];
+let computerChoice = hands[Math.floor(Math.random() * hands.length)];
+return computerChoice;
 };
 
 let computerHand = computerPlay();
 
-// console.log(computerHand)
+console.log(`The computer chose: ${computerHand}`)
 
 
-const playerSelection = function(){
-   let playerChoice = prompt("Choose: Rock, Paper, or Scissors!")
-   return playerChoice
+function gamePlay(playerHand, computerHand){
+    if (computerHand === 'rock' && playerHand === 'scissor'){
+        console.log("Sorry, you've lost! Rock beats scissors!")
+    } else if (computerHand === 'scissors' && playerHand === 'paper'){
+        console.log("Sorry, you've lost! Scissors cuts paper!")
+    } else if (computerHand === 'paper' && playerHand === 'rock'){
+        console.log("Sorry, you've lost! Paper covers rock!")
+    } else if (computerHand === playerHand){
+        console.log("You've tied!")
+    } else {
+        console.log(`You won, ${playerHand} beats ${computerHand}!`)
+    };
 }
 
-let playerChoice = playerSelection();
-
-let playerHand = playerChoice.toLowerCase();
-
-
-// console.log(playerHand);
-
-if (computerHand === 'rock' && playerHand === 'scissor'){
-    alert("Sorry, you've lost! Rock beats scissors!")
-} else if (computerHand === 'rock' && playerHand === 'rock'){
-    alert("You've tied! You both picked rock!")   
-} else if (computerHand === 'scissors' && playerHand === 'paper'){
-    alert("Sorry, you've lost! Scissors cuts paper!")
-} else if (computerHand === 'scissors' && playerHand === 'scissors'){
-    alert("You've tied! You both picked scissors")
-} else if (computerHand === 'paper' && playerHand === 'rock'){
-    alert("Sorry, you've lost! Paper covers rock!")
-} else if (computerHand === 'paper' && playerHand === 'paper'){
-    alert("You've tied!")
-} else {
-    alert(`You won! ${playerHand} beats ${computerHand}!`)
-};
+gamePlay();
