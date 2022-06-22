d27e3c7ede846aab05f14ac71f56dfe39df1f271
Nicktheprogrammerfin <106001886+Nicktheprogrammerfin@users.noreply.github.com>
2022-06-21 20:57:15 -0500

    Update script.js
    added comments, changed console.log to alerts at the end

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let playerScore = 0;
 let computerScore = 0;
 let winScore = 3;
 
+//this sets the basis for the game. Round starts at 1, both scores are at 0, and winScore is the limit for the scores (best out of 5)
+
+
 function gamePlay(){
     let playerSelection = prompt("Choose: Rock, Paper, or Scissors!");
     let playerHand = playerSelection.toLowerCase();
@@ -30,13 +33,20 @@ function gamePlay(){
     } else if (computerHand === playerHand){
         console.log("You've tied!")
     } else {
-        console.log(`You won, ${playerHand} beats ${computerHand}!`)
+        console.log(`You won, ${playerHand} beats ${computerHand}!`) 
         playerScore++;
     };
     
     round++;
 
 };
+
+// this is the main game. function gamePlay asks the player to choose between the three choices, automatically converts any answer into lowercase
+// chooses the computer's hand randomly from an array, pulls the computer's choice out of the function and into the variable "computerHand"
+// goes through all possibilities of ties and losses through the use of if/else if, otherwise the player wins and returns the "you won" message
+// if the computer wins, it increases computerScore by 1, or the playerScore if the player wins
+// the last line of the code is to increase the round number, just to keep track of which round the game is on
+
 
 while ((playerScore < winScore) || (computerScore < winScore)){
     console.log(`You are on round ${round}`);
@@ -45,12 +55,15 @@ while ((playerScore < winScore) || (computerScore < winScore)){
     console.log(`The computer has ${computerScore} points!`)
 
     if (playerScore === winScore){
-        console.log("Congrats, you won the battle! Refresh to play again!")
+        alert("Congrats, you won the battle! Refresh to play again!");
     } else if (computerScore === winScore){
-        console.log("Ooo, Tough break! The computer has won the battle! Refresh to try again!")
+        alert("Ooo, Tough break! The computer has won the battle! Refresh to try again!");
     }
 
     if ((playerScore === winScore) || (computerScore === winScore)){
         break;
     }
 };
+
+// this code loops the actual game. As long as the player or computer score is less than 3 (winScore), it loops the code
+// when either the player or computer score reaches 3, it breaks the code, ending the game and alerting the appropriate code
